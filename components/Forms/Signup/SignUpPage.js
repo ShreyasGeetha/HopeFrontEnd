2edca5c8807f58d46bf4fca1336ff36bd63166a1
ShreyasGeetha <shreyasgeetha@gmail.com>
2021-12-12 10:19:11 +0530

    'productUpdate'

--- components/Forms/Signup/SignUpPage.js
+++ components/Forms/Signup/SignUpPage.js
@@ -19,7 +19,7 @@ const SignUpPage = () => {
                       
                     </form>
 
-                    <div className="mt-6">
+                    {/* <div className="mt-6">
                       <div className="relative">
                         <div className="absolute inset-0 flex items-center">
                           <div className="w-full border-t border-gray-300" />
@@ -74,7 +74,7 @@ const SignUpPage = () => {
                           </a>
                         </div>
                       </div>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
     </>
